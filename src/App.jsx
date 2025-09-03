7622dfb6511cf1e4cdca286306b967ba0a017069
Niteshupadhyay085 <niteshupadhayay927@gmail.com>
2025-09-03 23:24:10 +0530

    Updated App.jsx with new UI changes

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,22 @@ import "./App.css";
 function App() {
   const { data, loading, error } = useFetch("https://api.escuelajs.co/api/v1/products");
 
-  if (loading) return <h2>Loading...</h2>;
+
+  if (loading) {
+  return (
+    <div className="loading-container">
+      <h2>Loading...</h2>
+    </div>
+  );
+}
   if (error) return <h2>Error: {error}</h2>;
+
+  // Some random background colors
+  const colors = ["#7FFF00", "#8A2BE2", "#00FF7F", "#FF1493", "#1E90FF", "#ADFF2F", "#FF4500", "#9400D3"];
 
   return (
     <div className="App">
+<<<<<<< HEAD
       <h1>Product List</h1>
       <ul>
         {data.slice(0, 40).map((item) => (
@@ -17,8 +28,22 @@ function App() {
             <img src={item.images[0]} alt={item.title} height="100" width="100" />
             {item.title}
           </li>
+=======
+      <h1>Photos</h1>
+      <div className="photo-grid">
+        {data.slice(0, 1920).map((item, index) => (
+          <div className="photo-card" key={item.id}>
+            <div
+              className="placeholder"
+              style={{ backgroundColor: colors[index % colors.length] }}
+            >
+              600 x 600
+            </div>
+            <p>{item.title}</p>
+          </div>
+>>>>>>> 816dfe3 (Image card added)
         ))}
-      </ul>
+      </div>
     </div>
   );
 }
